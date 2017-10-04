5e80e5aae381132a82825bcf5c3257a0eca4dc46
Dr. Ramm <kotsareu@gmail.com>
2017-10-04 09:38:12 +0300

    bend: fast refactoring

--- backend.py
+++ backend.py
@@ -14,30 +14,35 @@ GPIO_STAT = 0
 
 air_temp_path = 'air_temp'
 floor_delta_path = 'floor_delta'
+relay_switch_path = 'relay_switch'
 
-try:
+ds_1_path = '/sys/bus/w1/devices/28-0516814060ff/w1_slave'
+ds_2_path = '/sys/bus/w1/devices/28-05169410e9ff/w1_slave'
+
+try: 
     air = open(air_temp_path, 'r')
 except:
-    print 'air file doesnt exist'
-
-AIR_TEMP=air.read()
-
-if str(AIR_TEMP) == "":
-    AIR_TEMP = 22
-
-try:
+    print '\n air file doesnt exist'
+	
+try: 
     floor = open(floor_delta_path, 'r')
 except:
-    print 'floor file doesnt exist'
-
+    print '\n floor file doesnt exist'
+	
+# get temp from last tg command
+AIR_TEMP=air.read()
 FLOOR_DELTA = floor.read()
 
+if str(AIR_TEMP) == '':
+    AIR_TEMP = 23
 if str(FLOOR_DELTA) == '':
     FLOOR_DELTA = 1.5
 
 # define sensors for DHT and BMP
 sensor_dht = Adafruit_DHT.DHT22
 sensor_bmp = BMP085.BMP085()
+
+#initial stuff
 temp_bmp = 0
 press_bmp = 0
 DS_1 = 0
@@ -55,21 +60,19 @@ def get_values():
 
         humid_dht, temp_dht = Adafruit_DHT.read_retry(sensor_dht, DHT_PIN)
         temp_bmp=sensor_bmp.read_temperature()
-        press_bmp=sensor_bmp.read_pressure()*0.0075006375541921
-        DS_1 = 0
-        DS_2 = 0
+        press_bmp=sensor_bmp.read_pressure() * 0.0075006375541921
 
         def temp1():
             global DS_1
-            tempfile1 = open("/sys/bus/w1/devices/28-0516814060ff/w1_slave")
+            tempfile1 = open(ds_1_path)
             thetext1 = tempfile1.read()
             tempfile1.close()
             tempdata1 = thetext1.split("\n")[1].split(" ")[9]
-            DS_1 = float(tempdata1[2:])/1000
+            DS_1 = float(tempdata1[2:]) / 1000
 
         def temp2():
             global DS_2
-            tempfile2 = open("/sys/bus/w1/devices/28-05169410e9ff/w1_slave")
+            tempfile2 = open(ds_2_path)
             thetext2 = tempfile2.read()
             tempfile2.close()
             tempdata2 = thetext2.split("\n")[1].split(" ")[9]
@@ -88,16 +91,28 @@ def get_values():
 
 def get_relay_status():
     global RELAY_STATUS
-    f = open('relay_switch', 'r')
+    f = open(relay_switch_path, 'r')
     RELAY_STATUS = f.read()
+	if str(RELAY_STATUS) == "":
+        print "\n RELAY MODE STATUS is empty, using AUTO"
+        RELAY_STATUS = 'auto'
+	
 def get_floor_delta():
     global FLOOR_DELTA
     f = open(floor_delta_path, 'r')
     FLOOR_DELTA = f.read()
+	if str(FLOOR_DELTA) == '':
+        print '\n FLOOR_DELTA is empty, using stock'
+        FLOOR_DELTA = 1.5
+	
 def get_air_temp():
     global AIR_TEMP
     f = open(air_temp_path, 'r')
     AIR_TEMP = f.read()
+	if str(AIR_TEMP) == '':
+        print '\n AIR_TEMP is empty, using stock'
+        AIR_TEMP = 23
+	
 def enable_relay():
     global GPIO_STAT
     if GPIO_STAT == 0:
@@ -124,23 +139,11 @@ while True:
 
     DS_DELTA = DS_2 - DS_1
     TEMP_AVR = (temp_bmp + temp_dht) / 2
-    if str(RELAY_STATUS) == "":
-        print "\n RELAY MODE STATUS is empty, using AUTO"
-        RELAY_STATUS = 'auto'
-
-    if str(AIR_TEMP) == '':
-        print '\n AIR_TEMP is empty, using stock'
-        AIR_TEMP = 23
-
-    if str(FLOOR_DELTA) == '':
-        print '\n FLOOR_DELTA is empty, using stock'
-        FLOOR_DELTA = 1.5
-
+	
     print ("\n DS DELTA " + str(DS_DELTA) + " vs FLOOR DELTA " + str(FLOOR_DELTA)
             + "\n TEMP AVR " + str(TEMP_AVR) + " vs AIR TEMP " + str(AIR_TEMP)
             + "\n RELAY MODE STATUS " + str(RELAY_STATUS)
             + " \n RELAY " + str(GPIO_STAT))
-
 
     if str(RELAY_STATUS) == 'on':
         enable_relay()
@@ -151,5 +154,4 @@ while True:
             enable_relay()
         else:
             disable_relay()
-
     time.sleep(2)
